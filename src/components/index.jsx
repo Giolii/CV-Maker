c4afbd40320f51a09dcf40884ef6d0824ff415d5
Gi <160989051+Giolii@users.noreply.github.com>
2024-09-23 15:36:49 -0700

    fixed experience name

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,4 @@
 export { Infos } from "./Infos";
 export { Education } from "./Education";
-export { Experience } from "./experience";
+export { Experience } from "./Experience";
 export { ImageUploader } from "./ImageUploader";
